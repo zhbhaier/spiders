ba0d3721f077ab815712814de0e3473c8b0b6986
海尔 <2242608601@qq.com>
2024-12-31 11:39:32 +0800

    第二次（初学者）

--- 淘宝/taobao.py
+++ 淘宝/taobao.py
@@ -1,51 +1,82 @@
 import time
-
+import pandas as pd
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-# 全局变量
-keyword = input("请输入搜索商品名称: ")
 
-# 启动ChromeDriver服务
-options = webdriver.ChromeOptions()
+def setup_driver():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    driver = webdriver.Chrome(options=options)
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument",
+                           {"source": """Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"""})
+    driver.maximize_window()
+    return driver
 
-# 关闭自动测试状态显示 // 会导致浏览器报：请停用开发者模式
-options.add_experimental_option("excludeSwitches", ['enable-automation'])
-
-# 把chrome设为selenium驱动的浏览器代理；
-driver = webdriver.Chrome(options=options)
-
-# 反爬机制（此方法执行时可以注入一段 javascript 代码，这里 source 的值为改注入脚本）
-driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument",
-                       {"source": """Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"""})
-
-driver.get('https://www.taobao.com')
-
-# 窗口最大化
-driver.maximize_window()
-
-# wait是Selenium中的一个等待类，用于在特定条件满足之前等待一定的时间(这里是15秒)。
-# 如果一直到等待时间都没满足则会捕获TimeoutException异常
-wait = WebDriverWait(driver, 10)
-
-
-# 打开页面后会强制停止10秒，请在此时手动扫码登陆
-
-
-def main():
-    params(keyword)
-
-
-def params(keywords):
-    print(f"正在搜索{keywords}，获取页面内容......")
+def search_keyword(driver, keyword):
+    driver.get('https://www.taobao.com')
+    wait = WebDriverWait(driver, 10)
     key = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#q")))
     submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_TSearchForm > div.search-button > button')))
-    key.send_keys(keywords)
+    key.send_keys(keyword)
     submit.click()
     time.sleep(20)
-    print(driver.page_source)
 
+def scroll_and_load(driver):
+    for _ in range(20):  # 假设滚动5次
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)  # 等待加载
+
+def extract_data(driver):
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    items = soup.find_all('div', class_='doubleCard--gO3Bz6bu')
+    data_list = []
+
+    for index, item in enumerate(items, start=1):
+        title = item.find('div', class_='title--qJ7Xg_90').get_text(strip=True) if item.find('div', class_='title--qJ7Xg_90') else ''
+        price = item.find('span', class_='priceInt--yqqZMJ5a').get_text(strip=True) if item.find('span', class_='priceInt--yqqZMJ5a') else ''
+        deal = item.find('span', class_='realSales--XZJiepmt').get_text(strip=True) if item.find('span', class_='realSales--XZJiepmt') else ''
+        location = item.find('div', class_='procity--wlcT2xH9').get_text(strip=True) if item.find('div', class_='procity--wlcT2xH9') else ''
+        shop = item.find('span', class_='shopNameText--DmtlsDKm').get_text(strip=True) if item.find('span', class_='shopNameText--DmtlsDKm') else ''
+        url = item.find('a', href=True)['href'] if item.find('a', href=True) else ''
+        shop_url = item.find('a', class_='shopName--hdF527QA')['href'] if item.find('a', class_='shopName--hdF527QA') else ''
+        
+        img_tag = item.find('img', class_='iconPic--pZyhTNNV')
+        img_url = img_tag['src'] if img_tag and 'src' in img_tag.attrs else ''
+
+        data = {
+            "Page": 1,  # 假设为第一页
+            "Num": index,
+            "title": title,
+            "price": price,
+            "deal": deal,
+            "location": location,
+            "shop": shop,
+            "sPostFre": "包邮",  # 假设为包邮
+            "url": url,
+            "shop_url": shop_url,
+            "img_url": img_url
+        }
+
+        data_list.append(data)
+    
+    return data_list
+
+def save_to_excel(data_list):
+    df = pd.DataFrame(data_list)
+    df.to_excel('products.xlsx', index=False)
+    print("数据已保存到 products.xlsx")
+
+def main():
+    keyword = input("请输入搜索商品名称: ")
+    driver = setup_driver()
+    search_keyword(driver, keyword)
+    scroll_and_load(driver)
+    data_list = extract_data(driver)
+    save_to_excel(data_list)
+    driver.quit()
 
 if __name__ == '__main__':
     main()
